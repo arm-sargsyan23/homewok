5135d094b60f778157ddb5363f4584c9fbe8e48b
Armen <armenjan2310@gmail.com>
2023-10-10 19:53:30 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,75 @@
+
+/**1.1 */
+
+// let str = "Hello world"
+// console.log(str);
+
+/**1.2 */
+
+// let str1 = "Hello"
+// let str2 = "World"
+// console.log(str1 + " " + str2);
+
+/**1.3 */
+
+// const name = "Armen"
+// console.log("Hello" + " " + name); 
+
+/**1.4 */
+
+// const age = "16"
+// console.log("I am" + " " + age + " " + "years old"); 
+
+/**1.5 */
+
+// const hours = 19
+// const minutes = 25
+// const seconds = 41
+// console.log("ժամ" + ":"  + hours + " " + "րոպե" + ":"  + minutes + " " + "վայրկյան" + ":"  + seconds );
+
+/**2.1 */
+
+// let name = prompt("Your name please")
+// console.log ("Your name is" + " " + name);
+
+/**2.2 */
+
+// let num = prompt("any number please")
+// num **= 2
+// console.log(num);
+
+/**2.3 */
+
+// let mother = parseInt(prompt("Age your Mother"))
+// let father = parseInt(prompt("Age your Father"))
+// console.log(mother + father);
+
+/**2.4 */
+
+// let num = 100 + 200;
+// console.log(num);
+
+/**2.5 */
+
+// let num = parseFloat(prompt("any number"))
+// let float = 2.78
+// console.log(num + float);
+
+/** "*" */
+
+// let num_second = parseFloat(prompt("input any number in second please"))
+// let const_second = 60
+// num_second /= const_second
+// console.log(num_second);
+
+/** 3.1 */
+
+// let str = "Armen"
+// console.log(str[0]);
+// console.log(str[1]);
+
+/** 3.2 */
+
+// let num = "345"
+// parseInt(num)
+// console.log(num[0] * num[1] * num[2])

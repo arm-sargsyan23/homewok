9c49414df584cf16ddd54817dc18bc7bc67345f9
Armen <armenjan2310@gmail.com>
2023-10-22 21:57:19 +0400

    Add: data,js file, css styles, users and yourself info, deleted: logos files and favicon file

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import dataBase from "./lib/data"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+  render(){
+    const users = dataBase.users
+
+    return <div className="container">
+      <div className="user-container">
+        {users.map( user => {
+          return <div className="info-container" key={user.id}>
+            <div className="user-decoration">
+              <h4><strong>Name:</strong></h4><h4>{user.name}</h4>
+            </div>
+            
+            <div className="user-decoration">
+              <h4><strong>Email:</strong></h4><h4>{user.email}</h4>
+            </div>
+
+            <div className="user-decoration">
+              <h4><strong>City:</strong></h4><h4>{user.address.city}</h4>
+            </div>
+
+            <div className="user-decoration">
+              <h4><strong>Phone:</strong></h4><h4>{user.phone}</h4>
+            </div>
+
+            <div className="user-decoration">
+              <h4><strong>Webside:</strong></h4><h4>{user.website}</h4>
+            </div>
+
+            <div className="user-decoration">
+              <h4><strong>Company:</strong></h4><h4>{user.company.name}</h4>
+            </div>
+          </div>
+        })}
+      </div>
     </div>
-  );
+  }
 }
 
-export default App;
+export default App
